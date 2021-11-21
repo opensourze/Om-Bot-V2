21185a5ad30b0432007f3a12e1558f82740d7a3f
Omcodez1503 <76506512+Omcodez1503@users.noreply.github.com>
2021-11-21 13:56:26 -0500

    Add files via upload

---
+++ Om_UtilitiesV2/Om_UtilitiesV2/README.md
@@ -0,0 +1,16 @@
+<div align=right>
+<img src="https://img.shields.io/discord/884078410010333235?label=Server&logo=discord&logoColor=white&style=for-the-badge">
+</div>
+
+## OmUtilities - One bot, several uses.
+
+[![Banner](https://OmUtilities.netlify.app/banner.png)](https://OmUtilities.netlify.app/)
+
+OmUtilities is a multipurpose bot that has all the moderation tools you need, lots of useful utility commands, and a bunch of fun commands that we all need once in a while.
+
+- [Add the bot to your server](https://dsc.gg/OmUtilities)
+- [Website (with command list)](https://OmUtilities.netlify.app)
+- [Support and Updates Server](https://discord.gg/JGcnKxEPsW)
+- [Top.gg page (upvote OmUtilities!)](https://top.gg/bot/884080176416309288)
+
+## OmUtilities is NOT meant to be a self-hosted bot. The code is not optimised for customisation/self-hosting.
---
+++ Om_UtilitiesV2/Om_UtilitiesV2/main.py
@@ -0,0 +1,91 @@
+"This is the file to run. It contains the commands to warn/ban users from OmUtilities and background tasks."
+
+import os
+os.chdir(r"C:\Users\ombha\Desktop\Om_UtilitiesV2\Om_UtilitiesV2\cogs")
+
+import discord
+import topgg
+import dotenv
+from discord.ext import commands, tasks
+from discord_slash import SlashCommand
+
+from client import Client
+from utils import cluster
+
+bans = cluster["OmUtilities"]["bans"]
+
+dotenv.load_dotenv()
+
+client = Client()
+slash = SlashCommand(client, sync_commands=True, delete_from_unused_guilds=True)
+
+
+# Command to message a user from OmUtilities
+@client.command(hidden=True)
+@commands.is_owner()
+async def messageuser(ctx, id: int, *, message):
+    if id in client.owner_ids:
+        return await ctx.send("You can't send messages to an owner through OmUtilities.")
+    try:
+        user: discord.User = client.get_user(id)
+
+        embed = discord.Embed(
+            title="My developers have sent you a message!",
+            colour=client.colour,
+        )
+
+        embed.add_field(name="Message", value=message)
+
+        await user.send(embed=embed)
+        await ctx.send(f"✅ Messaged user `{user.name}` with this embed.", embed=embed)
+
+    except Exception as e:
+        await ctx.send(f"❌ **Error:**\n\n{e}")
+
+
+# Command to ban a user from submitting suggestions
+@client.command(hidden=True, aliases=["suggestionban", "suggestionblock"])
+@commands.is_owner()
+async def block(ctx, id: int, *, reason):
+    if id in client.owner_ids:
+        return await ctx.send("You can't block an owner.")
+    try:
+        user: discord.User = client.get_user(id)
+
+        embed = discord.Embed(
+            title="You've been blocked from sending suggestions!",
+            colour=0xFF0000,
+            description=f"You've been banned from submitting suggestions as we have noticed that you are spamming them.",
+        )
+        embed.add_field(name="Reason", value=reason, inline=False)
+
+        bans.insert_one({"_id": id})
+
+        await user.send(embed=embed)
+        await ctx.send(f"✅ Blocked user `{user.name}` with this embed:", embed=embed)
+
+    except Exception as e:
+        await ctx.send(f"❌ **Error:**\n\n{e}")
+
+
+# Update Top.gg stats every 30 minutes
+@tasks.loop(minutes=30)
+async def update_stats():
+    try:
+        await client.topggpy.post_guild_count()
+    except:
+        pass
+
+
+update_stats.start()
+
+if __name__ == "__main__":
+    # Loop through py files in cogs directory and load them
+    for file in os.listdir(r"C:\Users\ombha\Desktop\Om_UtilitiesV2\Om_UtilitiesV2\cogs"):
+        if file.endswith(".py"):
+            client.load_extension(f"cogs.{file[:-3]}")
+
+    # Load Jishaku
+    client.load_extension("jishaku")
+
+    client.run(os.environ["TOKEN"])
